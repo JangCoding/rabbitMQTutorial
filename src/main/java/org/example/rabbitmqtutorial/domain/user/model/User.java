c0278f6199b617ee5fbcc9e6405043b3a45c5295
JangCoding <jjangga813@naver.com>
2025-05-08 11:03:32 +0900

    feat : User service -기본 CRUD 작성

--- src/main/java/org/example/rabbitmqtutorial/domain/user/model/User.java
+++ src/main/java/org/example/rabbitmqtutorial/domain/user/model/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -12,6 +13,7 @@ import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 @Table(name = "users")
 public class User {
--- src/main/java/org/example/rabbitmqtutorial/domain/user/repository/UserRepository.java
+++ src/main/java/org/example/rabbitmqtutorial/domain/user/repository/UserRepository.java
@@ -4,7 +4,9 @@ import org.example.rabbitmqtutorial.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<User> findUserByUserId(Long userId);
 }
