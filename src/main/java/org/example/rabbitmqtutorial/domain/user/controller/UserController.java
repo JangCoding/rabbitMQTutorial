16ccb93ea5d8ce360ca82d4b79508e8f8d577cc2
JangCoding <jjangga813@naver.com>
2025-05-08 13:25:02 +0900

    feat : User Controller -유저 생성, 조회 작성

--- src/main/java/org/example/rabbitmqtutorial/domain/user/controller/UserController.java
+++ src/main/java/org/example/rabbitmqtutorial/domain/user/controller/UserController.java
@@ -1,11 +1,37 @@
 package org.example.rabbitmqtutorial.domain.user.controller;
 
+import org.example.rabbitmqtutorial.domain.user.dto.UserCreateRequest;
+import org.example.rabbitmqtutorial.domain.user.dto.UserResponse;
 import org.example.rabbitmqtutorial.domain.user.service.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
-    private UserService userService;
+    private final UserService userService;
+
+    public UserController(UserService userService)  // @RequiredArgsConstructor 로 대체 가능
+    {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public UserResponse createUser(@RequestBody UserCreateRequest request){
+        return userService.createUser(request);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<UserResponse> getUser(@PathVariable Long userId) {
+        UserResponse userResponse = userService.getUser(userId);
+        return ResponseEntity.ok(userResponse);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        List<UserResponse> userResponseList = userService.getAllUsers();
+        return ResponseEntity.ok(userResponseList);
+    }
 }
