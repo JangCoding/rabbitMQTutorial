2d5a0a5debe63291ea337dbb500e9e6225c7c4fb
JangCoding <jjangga813@naver.com>
2025-05-08 13:21:35 +0900

    feat : User Repository -Optional<T> 로 수정

--- src/main/java/org/example/rabbitmqtutorial/domain/user/repository/UserRepository.java
+++ src/main/java/org/example/rabbitmqtutorial/domain/user/repository/UserRepository.java
@@ -3,7 +3,6 @@ package org.example.rabbitmqtutorial.domain.user.repository;
 import org.example.rabbitmqtutorial.domain.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
