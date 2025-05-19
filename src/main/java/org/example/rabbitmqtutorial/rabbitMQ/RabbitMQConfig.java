a1b3ef0a91baaebf301d8d87c38f079b8a2dd7ca
JangCoding <jjangga813@naver.com>
2025-05-19 17:50:06 +0900

    feat : MQ Config 작성 -converter, Template, que, exchange, binding

--- src/main/java/org/example/rabbitmqtutorial/rabbitMQ/RabbitMQConfig.java
+++ src/main/java/org/example/rabbitmqtutorial/rabbitMQ/RabbitMQConfig.java
@@ -1,6 +1,10 @@
 package org.example.rabbitmqtutorial.rabbitMQ;
 
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,13 +12,58 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RabbitMQConfig {
 
-    public static final String DIRECT_QUEUE = "queue.direct";
-    public static final String DIRECT_EXCHANGE = "exchange.direct";
-    public static final String DIRECT_KEY = "routing.direct";
+    @Bean
+    public MessageConverter jsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+    }
+
+    // Email 전용 큐
+    public static final String EMAIL_QUEUE = "email.queue";
+    public static final String EMAIL_EXCHANGE = "email.exchange";
+    public static final String EMAIL_ROUTING_KEY = "email.routing.key";
+
+    @Bean
+    public Queue emailQueue(){
+        return new Queue(EMAIL_QUEUE);
+    }
+
+    @Bean
+    public DirectExchange emailExchange(){
+        return new DirectExchange(EMAIL_EXCHANGE);
+    }
+
+    @Bean
+    public Binding emailBinding(Queue emailQueue, DirectExchange emailExchange){
+        return BindingBuilder
+                .bind(emailQueue)
+                .to(emailExchange)
+                .with(EMAIL_ROUTING_KEY);
+    }
+
+
+
+
+
+
+
+
+
 
 
     // === [1] Direct Exchange ===
     // 정확한 Routing Key로만 메시지를 전달하는 방식
+
+    public static final String DIRECT_QUEUE = "queue.direct";
+    public static final String DIRECT_EXCHANGE = "exchange.direct";
+    public static final String DIRECT_KEY = "routing.direct";
+
     @Bean
     public Queue directQueue(){                 // amqp.core.Queue
         return new Queue(DIRECT_QUEUE);
